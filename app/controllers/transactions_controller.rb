e958bed10e3d70f1a059e849f0ccb1cd5cb8befc
chinhle <chinhle@chinhs-mbp.attlocal.net>
2019-08-26 17:17:52 -0400

    Complete successful PMD transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -17,37 +17,16 @@ class TransactionsController < ApplicationController
       payment_token = params[:payment_method_token]
       total_charge = params[:total_charge].to_i * 100
       retain = params[:save_card]
-      transaction_token = retrieve_transaction_token(purchase_uri, payment_token, total_charge, retain)
+      purchase_transaction_token = retrieve_transaction_token(purchase_uri, payment_token, total_charge, retain)
 
-      @transaction.transaction_token = transaction_token
+      @transaction.transaction_token = purchase_transaction_token
       @transaction.total_charge = @flight.price
       @transaction.save
-      flash.notice = "Transaction #{transaction_token} Completed!"
+      flash.notice = "Transaction #{purchase_transaction_token} Completed!"
 
-      if total_charge != @flight.price
-        pmd_delivery_uri = URI("https://core.spreedly.com/v1/receivers/#{Rails.configuration.spreedly['receiver_token']}/deliver.json")
-        pmd_delivery_post_request = Net::HTTP::Post.new(pmd_delivery_uri)
-
-        pmd_delivery_post_request.basic_auth(
-          Rails.configuration.spreedly['environment_key'],
-          Rails.configuration.spreedly['access_secret']
-        )
-
-        pmd_delivery_post_request['Content-Type'] = 'application/json'
-        pmd_delivery_post_request.body = {
-          'delivery' => {
-            'payment_method_token' => payment_token,
-            'url' => 'https://spreedly-echo.herokuapp.com',
-            'headers' => 'Content-Type: application/json',
-            'body' => '{ \"product_id\": \"916598\", \"card_number\": \"{{credit_card_number}}\" }'
-          }
-        }.to_json
-
-        pmd_delivery_response = Net::HTTP.start(pmd_delivery_uri.hostname, pmd_delivery_uri.port, use_ssl: true) do |http|
-          http.request(pmd_delivery_post_request).body
-        end
-        parsed_pmd_delivery_response = JSON.parse(pmd_delivery_response)
-        pmd_transaction_token = parsed_pmd_delivery_response['transaction']['token']
+      if total_charge != @flight.price * 100
+        pmd_transaction_token = retrieve_transaction_token(pmd_uri, payment_token, 0, retain)
+        flash.notice = "PMD transaction #{pmd_transaction_token} Completed!"
       end 
     end
   
--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -4,12 +4,13 @@ module TransactionsHelper
 
   def retrieve_transaction_token(uri, payment_token, amount, retain)
     transaction_request = transaction_post_request(uri, payment_token, amount, retain)
-    purchase_response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+
+    transaction_response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
       http.request(transaction_request).body
     end
 
-    parsed_purchase_response = JSON.parse(purchase_response)
-    parsed_purchase_response['transaction']['token']
+    parsed_transaction_response = JSON.parse(transaction_response)
+    parsed_transaction_response['transaction']['token']
   end
 
   def transaction_post_request(transaction_uri, payment_token, amount, retain)
@@ -21,7 +22,13 @@ module TransactionsHelper
     )
 
     request['Content-Type'] = 'application/json'
-    request.body = purchase_data(payment_token, amount, retain).to_json
+    
+    if transaction_uri.to_s.include? 'purchase.json'
+      request.body = purchase_data(payment_token, amount, retain).to_json
+    else
+      request.body = pmd_data(payment_token).to_json
+    end
+
     request
   end
 
@@ -36,8 +43,25 @@ module TransactionsHelper
     }
   end
 
+  def pmd_data(payment_token)
+    {
+      'delivery' => {
+        'payment_method_token' => payment_token,
+        'url' => 'https://spreedly-echo.herokuapp.com',
+        'headers' => 'Content-Type: application/json',
+        'body' => '{ \"product_id\": \"916598\", \"card_number\": \"{{credit_card_number}}\" }'
+      }
+    }
+  end
+
   def purchase_uri
     # build the cURL command mentioned here: https://docs.spreedly.com/basics/purchase/
     URI("https://core.spreedly.com/v1/gateways/#{Rails.configuration.spreedly['gateway_token']}/purchase.json")
   end
+ 
+  def pmd_uri
+    # build the cURL command mentioned here: 
+    # https://docs.spreedly.com/guides/payment-method-distribution/single-card/#deliver-payment-method
+    URI("https://core.spreedly.com/v1/receivers/#{Rails.configuration.spreedly['receiver_token']}/deliver.json")
+  end 
 end
